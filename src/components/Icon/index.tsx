4cdf323b49608b0bdadb49c2d3230089ac0bb45c
songzheming <songzheming@163.com>
2023-07-27 15:06:39 +0800

    update: common icon add svg icon

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,35 @@
+import * as Icons from '@/shared/icons';
+export const Icon: React.FC<{
+  width: number;
+  src?: string;
+  invert: boolean;
+  icon?: string;
+}> = props => {
+  if (props.src) {
+    const src = `imgs/icon/${props.src}.png`;
+    return (
+      <div className="mx-2 relative grid place-items-center ">
+        <img
+          className={props.invert ? 'invert' : ''}
+          style={{
+            width: props.width,
+          }}
+          src={src}
+        />
+      </div>
+    );
+  }
+  if (props.icon) {
+    const CustomIcon = Icons[props.icon];
+    return (
+      <div className="mx-2 relative grid place-items-center ">
+        <CustomIcon
+          className={props.invert ? 'invert' : ''}
+          style={{
+            width: props.width,
+          }}
+        />
+      </div>
+    );
+  }
+};
---
+++ src/shared/icons.tsx
@@ -0,0 +1,11 @@
+export const taskSearch: React.FC<NonNullable<unknown>> = props => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -1 26 26" height="28" width="28">
+      <circle cx="13" cy="11" r="8" fill="#ffffff24"></circle>
+      <g fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="2">
+        <circle cx="13" cy="11" r="8"></circle>
+        <path d="M3 21l4-4" strokeLinecap="round"></path>
+      </g>
+    </svg>
+  );
+};
