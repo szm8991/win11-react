e95fa043492aa735997fdf124dbbcddc99f91287
songzheming <songzheming@163.com>
2023-08-03 15:40:27 +0800

    update: desktop taskbar

--- src/pages/Desktop/components/Taskbar/index.tsx
+++ src/pages/Desktop/components/Taskbar/index.tsx
@@ -1,47 +1,54 @@
 import { Battery } from '@/components/Battery';
 import { Icon } from '@/components/Icon';
-import { useState } from 'react';
+import { MouseEventHandler, useState } from 'react';
 import './index.scss';
 export const Taskbar = () => {
   const [time, setTime] = useState(new Date());
+
+  const handler: MouseEventHandler<HTMLDivElement> = e => {
+    if (!(e.currentTarget instanceof HTMLDivElement)) {
+      return;
+    }
+    e.currentTarget.dataset.open = 'true';
+    // e.target.dataset.active = true;
+  };
   return (
-    <div className="taskbar min-w-full h-12 absolute bottom-0 flex justify-between items-center px-3">
-      <div>
-        <Icon width={24} src="widget" invert={false} />
-      </div>
-      <div className="flex gap-2">
-        <Icon width={24} src="home" invert={false} />
-        {/* <Icon icon="taskSearch" invert={false} /> */}
-        <Icon width={24} src="search" invert />
-        <Icon width={24} src="settings" invert={false} />
-        <Icon width={24} src="explorer" invert={false} />
-        <Icon width={24} src="edge" invert={false} />
-        <Icon width={24} src="store" invert={false} />
-        <Icon width={24} src="terminal" invert={false} />
-      </div>
-      <div className="flex flex-row">
-        <Icon icon="upArrow" invert={false} />
-        <Icon width={16} src="audio3" invert />
-        <Icon width={16} src="wifi" invert />
-        <div className="mx-2 relative grid place-items-center">
-          <Battery invert />
+    <>
+      <div className="taskbar min-w-full h-12 absolute bottom-0 flex justify-between items-center px-3">
+        <div className="center absolute bottom-0 min-w-full flex h-12 justify-center">
+          <Icon width={24} src="widget" invert={false} className="task-icon icon-1" />
+          <Icon width={24} src="home" invert={false} className="task-icon" onClick={handler} />
+          <Icon width={24} src="search" invert className="task-icon" />
+          <Icon width={24} src="settings" invert={false} className="task-icon" />
+          <Icon width={24} src="explorer" invert={false} className="task-icon" />
+          <Icon width={24} src="edge" invert={false} className="task-icon" />
+          <Icon width={24} src="store" invert={false} className="task-icon" />
+          <Icon width={24} src="terminal" invert={false} className="task-icon" />
         </div>
-        <div className="task-date">
-          <div>
-            {time.toLocaleTimeString('en-US', {
-              hour: 'numeric',
-              minute: 'numeric',
-            })}
+        <div className="right absolute right-0 flex flex-row">
+          <Icon icon="upArrow" invert={false} />
+          <Icon width={16} src="audio3" invert />
+          <Icon width={16} src="wifi" invert />
+          <div className="mx-1 relative grid place-items-center">
+            <Battery invert />
           </div>
-          <div>
-            {time.toLocaleDateString('en-US', {
-              year: '2-digit',
-              month: '2-digit',
-              day: 'numeric',
-            })}
+          <div className="task-date">
+            <div>
+              {time.toLocaleTimeString('en-US', {
+                hour: 'numeric',
+                minute: 'numeric',
+              })}
+            </div>
+            <div>
+              {time.toLocaleDateString('en-US', {
+                year: '2-digit',
+                month: '2-digit',
+                day: 'numeric',
+              })}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
