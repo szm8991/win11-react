a8ca869a157ffb4bc0ebde263fe65dacf0bdab1c
songzheming <songzheming157@163.com>
2024-07-16 08:56:31 +0800

    chore: add prettier config, change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,10 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
+  files: ['*.ts', '*.tsx'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
@@ -18,10 +20,11 @@ module.exports = {
   },
   plugins: ['react-refresh'],
   rules: {
+    quotes: ['error', 'single'],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     '@typescript-eslint/no-non-null-assertion': 'off',
   },
-}
+};
